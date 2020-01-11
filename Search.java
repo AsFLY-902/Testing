b52d28fb13a5494b3d2473c85197ef5644ebdb61
AsFLY-902 <37661597+AsFLY-902@users.noreply.github.com>
2020-01-11 19:23:37 +0530

    Final

--- Search.java
+++ Search.java
@@ -1,14 +1,24 @@
+    /*
+    Created by Ashok for Grievance system on 11/01/2020
+    */
+
+
+    /**
+    The below function is implemented inside a manual Search box with a action button , which will search the user input within the complaints
+    node in Firebase Database.
+    */
 public class Search{
-    private EditText mSearchField;
-    private ImageButton mSearchBtn;
-	private RecyclerView mResultList;
+	
+	private EditText mSearchField;
+    	private ImageButton mSearchBtn;
+    	private RecyclerView mResultList;
 	private ReferenceAdapter mAdapter;
 	private final List<Reference> list =  new ArrayList<>();
-
-    private DatabaseReference mDatabase;
-    private Query query;
+	
+	private DatabaseReference mDatabase;
+    	private Query query;
  
-		mDatabase = FirebaseDatabase.getInstance().getReference().child("app").child("complaints");
+	mDatabase = FirebaseDatabase.getInstance().getReference().child("app").child("complaints");
 
 
         mResultList.setLayoutManager(new LinearLayoutManager(this));
@@ -26,15 +36,15 @@ public class Search{
     private void loadPosts(String title,String year,String cell,String description) {
 
         query = mDatabase.orderByChild("title").startAt(title).endAt(title+"\uf8ff");
-		query = mDatabase.orderByChild("year").startAt(title).endAt(year+"\uf8ff");
-		query = mDatabase.orderByChild("cell").startAt(title).endAt(cell+"\uf8ff");
-		query = mDatabase.orderByChild("description").startAt(title).endAt(description+"\uf8ff");
+	query = mDatabase.orderByChild("year").startAt(title).endAt(year+"\uf8ff");
+	query = mDatabase.orderByChild("cell").startAt(title).endAt(cell+"\uf8ff");
+	query = mDatabase.orderByChild("description").startAt(title).endAt(description+"\uf8ff");
 
         query.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Reference reference =  dataSnapshot.getValue(Reference.class);
-                list.add(reference);
+                list.add(reference);  //Final results will be shown (return value)
                 mAdapter.notifyDataSetChanged();
             }
 
